768c00c93a5b9df92d04631cefc5b7e19d9be1f8
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2026-02-15 16:51:14 +0530

    Delete checoutGit.groovy

--- checoutGit.groovy
+++
@@ -1,9 +0,0 @@
-def call(String gitUrl, String branch) {
-
-  checkout([
-    $class: 'GitSCM',
-    branches: [[name: branch]],
-    userRemoteConfigs: [[url: gitUrl]]
-  ])
-  sh 'mkdir reports'
-}
